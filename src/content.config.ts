5f9f0cf4a6cf014c572d8be6030389df7eb5cb61
Brian McFadden <101163132+bmcfads@users.noreply.github.com>
2026-05-15 13:35:47 -0700

    Update blog page

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,10 +10,11 @@ const blog = defineCollection({
 		z.object({
 			title: z.string(),
 			description: z.string(),
-			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: z.optional(image()),
+			featured: z.boolean().optional(),
+			tags: z.array(z.string()).optional(),
 		}),
 });
 
